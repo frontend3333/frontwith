4b60e75cc201e0a01213c9a70171fdf4b93d5010
ORG <47654977+ryeongee@users.noreply.github.com>
2021-05-25 02:32:49 +0900

    .
    .

--- Fin/1/src/exam/todos/main.js
+++ Fin/1/src/exam/todos/main.js
@@ -1,104 +1,104 @@
-// 코드 구현
-const todoForm = document.querySelector(".js-form");
-const todoInput = todoForm.querySelector("input");
-const todoList = document.querySelector(".todos");
+const TODOS_LS = "todos";
+let todos = [];
+let load = [];
 
-const TODOS_LS = "todosLs";
-let todosLs = [
-    {
-      id: 1,
-      text: "HTML",
-      completed: true,
-    },
-    {
-      id: 2,
-      text: "CSS",
-      completed: true,
-    },
-    {
-      id: 3,
-      text: "Javascript",
-      completed: false,
-    },
-];
+const $input = document.querySelector(".input-todo");
+const $todos = document.querySelector(".todos");
 
-function handleDelete(event) {
-  const li = event.target.parentNode; // li에 btn의 부모 태그(li)를 대입
-  todoList.removeChild(li); // todos태그의 자식에 있는 li(btn.parentNode)를 제거 // filter를 사용해서 return 결과가 true인 것들만 추출됨
-  const deleteTodos = todosLs.filter(function (todo) {
-    return todo.id !== parseInt(li.id);
+function render() {
+  let html = "";
+
+  todos.forEach((todo) => {
+    html += `
+    <li id=${todo.id}>
+      <label>
+        <input type="checkbox"  ${todo.completed ? "checked" : ""}>
+        ${todo.content}
+      </label>
+      <span class="remove">X</span>
+    </li>
+    `;
   });
-  todosLs = deleteTodos; // 추출된 내용을 todosLS에 넣음
-  saveTodos(); // localStorage에 저장
+  saveTodos();
+  $todos.innerHTML = html;
 }
 
 function saveTodos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(todosLs)); // localStorage에 리스트 저장
+  localStorage.setItem(TODOS_LS, JSON.stringify(todos)); // localStorage에 리스트 저장
 }
 
-function addTodos(text) {
-  const li = document.createElement("li"); // li 태그 생성
-  const label = document.createElement("label");
-  const input = document.createElement("input");
+// const getTodos = function () {
+//   // TODO: 서버로부터 데이터를 취득.
+//   todos = [
+//     { id: 1, content: "HTML", completed: true },
+//     { id: 2, content: "CSS", completed: true },
+//     { id: 3, content: "Javascript", completed: false },
+//   ];
+//   render();
+// };
 
-  const delBtn = document.createElement("span"); // button 태그 생성
-  const newId = todosLs.length + 1;
-  li.id = newId;
-  input.setAttribute("type", "checkbox");
-
-  delBtn.innerText = "X";
-
-  delBtn.addEventListener("click", handleDelete); // delBtn에 클릭이벤트에 deleteTodo함수 연결
-
-  li.appendChild(label); // li의 자식에 label 연결
-  li.appendChild(delBtn); // li의 자식에 delBtn 연결
-  label.append(input, text); // label의 자식에 input 연결(체크박스 앞으로감)
-  todoList.appendChild(li); // toDoList의 자식에 li 연결
-
-  todoInput.value = "";
-
-  const todoObj = {
-    id: newId,
-    text: text,
-    completed: false,
-  };
-
-  todosLs.push(todoObj); // todosLs에 todoObj 삽입
-  saveTodos(); // localStorage에 저장하는 함수
+function findMaxId() {
+  return Math.max(0, ...todos.map((todo) => todo.id));
 }
 
-function handleSubmit(event) {
-  event.preventDefault(); // 이벤트가 작동하지 못하게 함
-  const currentValue = todoInput.value; // currentValue에 input창에 입력한 값 대입
-  addTodos(currentValue); // 리스트 추가하는 함수
-}
-
-function loadList() {
-  const loadTodos = localStorage.getItem(TODOS_LS);
-  todoForm.addEventListener("submit", handleSubmit);
-  // localStorage에 todos_ls가 있는지 확인
-  if (loadTodos !== null) {
-    const parseTodo = JSON.parse(loadTodos); // loadToDos를 json객체로 변경
-    parseTodo.forEach(function (todo) {
-      addTodos(todo.text); // 리스트 추가하는 함수
-    }); // 객체 내용 한개씩 파라미터로 넣어서 함수 실행
+const addTodos = function (e) {
+  if(e.keyCode === 13 && $input.value.trim() !== ""){
+    todos = [
+      ...todos,
+      { id: findMaxId() + 1, content: $input.value, completed: false },
+    ];
+    render();
+    $input.value = "";
+    return false;
   }
-}
+};
+
+const removeTodos = function (e) {
+  if (!e.target.classList.contains("remove")) return;
+
+  const listId = +e.target.parentNode.id;
+  todos = todos.filter((todo) => todo.id !== listId);
+
+  // Id 재정렬
+  if (todos.length !== findMaxId()) {
+    todos = todos.map((todo, index) => ({ ...todo, id: index + 1 }));
+  }
+  render();
+};
+
+const toggleTodos = function (e) {
+  const listId = +e.target.parentNode.parentNode.id;
+  todos = todos.map((todo) =>
+    todo.id === listId ? { ...todo, completed: !todo.completed } : todo
+  );
+
+  render();
+};
 
 function loading(){
+  console.log($todos);
   const msg = "로딩중...";
   const loading_text = document.createElement("li");
   loading_text.setAttribute('id',msg);
   const textNode = document.createTextNode(msg);
   loading_text.appendChild(textNode);
-  todoList.appendChild(loading_text);
-  setTimeout(() => todoList.removeChild(loading_text),1000);
+  $todos.appendChild(loading_text);
+  setTimeout(() => $todos.removeChild(loading_text),1000);
 }
 
-function show() {
-  loading();
-  setTimeout(() => loadList(),1000);
-   // toDoForm에서 submit에 handleSubmit 이벤트를 연결
+function main(){
+  $input.onkeydown = addTodos;
+  $todos.onclick = removeTodos;
+  $todos.onchange = toggleTodos;
 }
 
-show();
+loading();
+window.onload = function () {
+  todos = [
+    { id: 1, content: "HTML", completed: true },
+    { id: 2, content: "CSS", completed: true },
+    { id: 3, content: "Javascript", completed: false },
+  ];
+  setTimeout(render, 1000);
+  setTimeout(main, 1000);
+};
--- Fin/1/src/exam/todos/mode.js
+++ Fin/1/src/exam/todos/mode.js
@@ -1,1 +1,39 @@
 // 코드 구현
+
+const $checkbox = document.querySelector(".dark-mode");
+// const $checkbox = document.querySelector(".light-mode");
+const modeState = document.getElementById('modeID');
+const isUserColorTheme = localStorage.getItem("color-theme");
+const isOsColorTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "light"
+  : "dark";
+
+const getUserTheme = () =>
+  isUserColorTheme ? isUserColorTheme : isOsColorTheme;
+
+window.onload = function () {
+  if (getUserTheme === "light") {
+    localStorage.setItem("color-theme", "light");
+    document.documentElement.setAttribute("color-theme", "light");
+    modeState.innerHTML = '<span class="mode-text">라이트모드</span>';
+  } else {
+    localStorage.setItem("color-theme", "dark");
+    document.documentElement.setAttribute("color-theme", "dark");
+    modeState.innerHTML = '<span class="mode-text">다크모드</span>';
+    $checkbox.setAttribute("checked", true);
+  }
+};
+
+$checkbox.addEventListener("click", (e) => {
+  if (e.target.checked) {
+    localStorage.setItem("color-theme", "light");
+    document.documentElement.setAttribute("color-theme", "light");
+    modeState.innerHTML = '<span class="mode-text">라이트모드</span>';
+  } else {
+    localStorage.setItem("color-theme", "dark");
+    document.documentElement.setAttribute("color-theme", "dark");
+    modeState.innerHTML = '<span class="mode-text">다크모드</span>';
+  }
+});
+
+console.log(isOsColorTheme);
