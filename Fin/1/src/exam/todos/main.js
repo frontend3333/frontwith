2791c316d8ab9800a974cff1f32ab370279bebf7
minjai94 <77010869+minjai94@users.noreply.github.com>
2021-05-26 12:02:00 +0900

    todos_final

--- Fin/1/src/exam/todos/main.js
+++ Fin/1/src/exam/todos/main.js
@@ -1,53 +1,51 @@
-const TODOS_LS = "todos";
-let todos = [];
-let load = [];
+/* eslint-disable */
+
+// 외부 js 파일 사용 시 window.onload 안 됨
+// https://github.com/codesandbox/codesandbox-client/issues/4683
 
 const $input = document.querySelector(".input-todo");
 const $todos = document.querySelector(".todos");
 
-function render() {
-  let html = "";
-
-  todos.forEach((todo) => {
-    html += `
-    <li id=${todo.id}>
-      <label>
-        <input type="checkbox"  ${todo.completed ? "checked" : ""}>
-        ${todo.content}
-      </label>
-      <span class="remove">X</span>
-    </li>
-    `;
-  });
-  saveTodos();
-  $todos.innerHTML = html;
+let todos = JSON.parse(localStorage.getItem("todos"));
+if (!todos) {
+  todos = [
+    { id: 1, content: "HTML", completed: true },
+    { id: 2, content: "CSS", completed: true },
+    { id: 3, content: "Javascript", completed: false },
+  ];
 }
 
 function saveTodos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(todos)); // localStorage에 리스트 저장
+  localStorage.setItem("todos", JSON.stringify(todos)); // localStorage에 리스트 저장
 }
 
 function findMaxId() {
   return Math.max(0, ...todos.map((todo) => todo.id));
 }
 
-const addTodos = function (e) {
+function addTodos(e) {
+  // * keyCode는 Deprecated 되었습니다.
+  // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
   if (e.code === "Enter" && $input.value.trim() !== "") {
-    todos = [
-      ...todos,
-      { id: findMaxId() + 1, content: $input.value, completed: false },
-    ];
+    todos.push({
+      id: findMaxId() + 1,
+      content: $input.value,
+      completed: false,
+    });
     render();
     $input.value = "";
     return false;
   }
-};
+}
 
 const removeTodos = function (e) {
   if (!e.target.classList.contains("remove")) return;
 
   const listId = +e.target.parentNode.id;
-  todos = todos.filter((todo) => todo.id !== listId);
+  todos.splice(
+    todos.findIndex((todo) => todo.id === listId),
+    1
+  );
 
   // Id 재정렬
   if (todos.length !== findMaxId()) {
@@ -58,37 +56,33 @@ const removeTodos = function (e) {
 
 const toggleTodos = function (e) {
   const listId = +e.target.parentNode.parentNode.id;
-  todos = todos.map((todo) =>
-    todo.id === listId ? { ...todo, completed: !todo.completed } : todo
-  );
+  const idx = todos.findIndex((todo) => todo.id === listId);
+  todos[idx].completed = !todos[idx].completed;
 
   render();
 };
 
-function loading() {
-  console.log($todos);
-  const msg = "로딩중...";
-  const loading_text = document.createElement("li");
-  loading_text.setAttribute("id", msg);
-  const textNode = document.createTextNode(msg);
-  loading_text.appendChild(textNode);
-  $todos.appendChild(loading_text);
-  setTimeout(() => $todos.removeChild(loading_text), 1000);
+function render() {
+  if ($todos) {
+    $todos.innerHTML = todos
+      .map(
+        (todo) =>
+          `<li id=${todo.id}>
+      <label>
+        <input type="checkbox"  ${todo.completed ? "checked" : ""}>
+        ${todo.content}
+      </label>
+      <span class="remove">X</span>
+    </li>`
+      )
+      .join("");
+    saveTodos();
+  }
 }
+setTimeout(render, 1000);
 
 if ($input && $todos) {
   $input.onkeypress = addTodos;
   $todos.onclick = removeTodos;
   $todos.onchange = toggleTodos;
 }
-
-loading();
-window.onload = function () {
-  todos = [
-    { id: 1, content: "HTML", completed: true },
-    { id: 2, content: "CSS", completed: true },
-    { id: 3, content: "Javascript", completed: false },
-  ];
-  setTimeout(render, 1000);
-  setTimeout(main, 1000);
-};
