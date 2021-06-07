4895b4878a6e29138c06e015a404518a786d5d51
ORG <47654977+ryeongee@users.noreply.github.com>
2021-06-07 09:46:00 +0900

    Update api.js

--- Fin/2/src/component/api.js
+++ Fin/2/src/component/api.js
@@ -6,7 +6,6 @@ const Kakao = axios.create({
     Authorization: "KakaoAK 38095ad2bbeec69dc46b2ec65238801d" // 공통으로 요청 할 헤더
   }
 });
-
 // search blog api
 export const bookSearch = (params) => {
   return Kakao.get("/v3/search/book", { params });
